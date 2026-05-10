4b74e1895defffd8f234f07b1288baa7abe1754e
RomsiVmax <romeo.seeger@online.de>
2026-05-10 16:59:49 +0200

    Update Installer.bat

--- The EM Project 4/Installer.bat
+++ The EM Project 4/Installer.bat
@@ -1,1 +1,21 @@
 @echo off
+echo Druecke eine Taste, wenn du Git installiert hast. Falls du Git nicht installiert hast, installiere es JETZT und druecke dann Enter.
+C:/Windows/System32/timeout.exe /t 10000
+cd %USERPROFILE%/curseforge/minecraft/Instances/
+mkdir "The EM Project 4"
+mkdir "The EM Temp"
+cd "The EM Temp"
+git clone https://github.com/RomsiVmax/Minecraft.git
+cd Minecraft
+cd "The EM Project 4"
+cd assets
+xcopy "config" "%USERPROFILE%/curseforge/minecraft/Instances/The EM Project 4/config" /S /E /C /I /R /H /Y
+xcopy "mods" "%USERPROFILE%/curseforge/minecraft/Instances/The EM Project 4/mods" /S /E /C /I /R /H /Y
+cd ..
+cd ..
+cd ..
+cd ..
+rmdir "The EM Temp" /S /Q
+echo Installation erfolgreich!
+echo Druecke eine Taste, um fortzufahren.
+C:/Windows/System32/timeout.exe /t 10000
